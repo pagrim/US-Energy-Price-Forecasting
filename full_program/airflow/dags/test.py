1d1f06f74042e7d0003f87d4b1fa1535d6702374
Peter Grimshaw <pagrimshaw@gmail.com>
2024-11-26 21:36:26 +0000

    Refactored modelling to use tf.data.Dataset, need to align data and model shape

--- full_program/airflow/dags/test.py
+++ full_program/airflow/dags/test.py
@@ -1,15 +1,12 @@
 import numpy as np
 from transformation.etl_transforms import EtlTransforms
+from modelling.model import Model
 
 np.random.seed(42)
 X_train = np.random.randn(200, 4)
 y_train = np.random.randn(200, 2)
 
 train_dataset = EtlTransforms.build_dataset(x=X_train, y=y_train, sequence_length=30, batch_size=128)
-
-# Example usage
-for batch_x, batch_y in train_dataset.take(1):
-    print("Batch X shape:", batch_x.shape)
-    print("Batch Y shape:", batch_y.shape)
-    print("Batch X:", batch_x)
-    print("Batch Y:", batch_y)
+model = Model()
+mdl = model.compile_model(time_steps=30)
+model._train(model=mdl, dataset=train_dataset, validation_dataset=train_dataset)
